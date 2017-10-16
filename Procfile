7e2fcfe0d5fde54f81f2badea8d3a83813d8dbc8
pal0r <ryanpalo@gmail.com>
2017-10-16 15:28:35 -0700

    Updates run scripts to nodemon and foreman

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: npm start
+api: nodemon index.js
--- config/main.js
+++ config/main.js
@@ -2,7 +2,7 @@ module.exports = {
   // JWT Secret Key for signing requests
   'secret': 'H8LaZPWgFDbXUYsbTwHacwZa',
   // Database connection data
-  'database': 'mongodb://localhost/test',
-  // 'database': 'mongodb://ryanpalo:pUNQ3)6g@ds153412.mlab.com:53412/flasherdb',
+  // 'database': 'mongodb://localhost/test',
+  'database': 'mongodb://ryanpalo:O65eiZlGCQ@ds153412.mlab.com:53412/flasherdb',
   'port': process.env.PORT || 3000
 }
