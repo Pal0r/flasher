5dc12ae77dc361f53014dbe4f6a85bf969f07e18
pal0r <ryanpalo@gmail.com>
2017-11-30 16:29:21 -0800

    Fix and add shim to fix broken react 16 tests

---
+++ config/shim.js
@@ -0,0 +1,7 @@
+// Temporary hack to suppress error
+// https://github.com/facebookincubator/create-react-app/issues/3199
+const raf = global.requestAnimationFrame = (cb) => {
+  setTimeout(cb, 0)
+}
+
+export default raf
